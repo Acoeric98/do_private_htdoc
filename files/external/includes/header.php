76c10ce9ead7b37b5307b19a70f506a3e522b5f9
Acoeric98 <98672535+Acoeric98@users.noreply.github.com>
2025-12-07 20:23:55 +0100

    Restore nav links and enlarge header layout

--- files/external/includes/header.php
+++ files/external/includes/header.php
@@ -24,41 +24,44 @@
         <div class="nav-wrapper grey darken-4">
           <div class="container nav-shell">
             <div class="nav-table">
-              <ul class="nav-column nav-column--left">
-                <li><a href="<?php echo DOMAIN; ?>ships">Hangar</a></li>
-                <li><a href="<?php echo DOMAIN; ?>equipment">Equipment</a></li>
-                <li><a href="<?php echo DOMAIN; ?>galaxy-gates">Galaxy Gates</a></li>
-                <li><a href="<?php echo DOMAIN; ?>pet-15">P.E.T 15</a></li>
-              </ul>
+              <a class="nav-cell nav-cell--hangar" href="<?php echo DOMAIN; ?>ships">Hangar</a>
+              <a class="nav-cell nav-cell--brand" href="<?php echo DOMAIN; ?>">
+                <div>Zero Tolerance PvP</div>
+                <span>Pilot ID: <?php echo $player['userId']; ?></span>
+              </a>
+              <a class="nav-cell nav-cell--shop" href="<?php echo DOMAIN; ?>shop">Shop</a>
 
-              <div class="nav-column nav-column--center">
-                <a href="<?php echo DOMAIN; ?>" class="brand-stacked">Zero-Tolerance PvP</a>
-                <div class="nav-meta">
-                  <span class="nav-meta__item">Pilot ID: <?php echo $player['userId']; ?></span>
-                </div>
-                <a class="btn nav-start grey darken-3 waves-effect waves-light" href="<?php echo DOMAIN; ?>map-revolution" target="_blank">Start</a>
-              </div>
+              <a class="nav-cell nav-cell--equipment" href="<?php echo DOMAIN; ?>equipment">Equipment</a>
+              <div class="nav-cell nav-cell--empty" aria-hidden="true"></div>
+              <div class="nav-cell nav-cell--empty" aria-hidden="true"></div>
+              <a class="nav-cell nav-cell--auction" href="<?php echo DOMAIN; ?>auction">Auction</a>
 
-              <ul class="nav-column nav-column--right">
-                <li><a href="<?php echo DOMAIN; ?>shop">Shop</a></li>
-                <li><a href="<?php echo DOMAIN; ?>auction">Auction</a></li>
-                <li><a href="<?php echo DOMAIN; ?>settings">Settings</a></li>
-                <li><a href="/api/logout">Logout</a></li>
-              </ul>
+              <a class="nav-cell nav-cell--pet" href="<?php echo DOMAIN; ?>pet-15">Pet</a>
+              <a class="nav-cell nav-cell--start" href="<?php echo DOMAIN; ?>map-revolution" target="_blank">Start</a>
+              <a class="nav-cell nav-cell--settings" href="<?php echo DOMAIN; ?>settings">Settings</a>
+
+              <a class="nav-cell nav-cell--galaxy" href="<?php echo DOMAIN; ?>galaxy-gates">Galaxy Gates</a>
+              <div class="nav-cell nav-cell--empty" aria-hidden="true"></div>
+              <div class="nav-cell nav-cell--empty" aria-hidden="true"></div>
+              <a class="nav-cell nav-cell--logout" href="/api/logout">Logout</a>
             </div>
 
-            <div class="nav-stats">
+            <div class="nav-stats-row">
               <div class="nav-stat">
-                <span class="nav-stat__label">Uridium</span>
-                <span class="nav-stat__value" id="uridium"><?php echo number_format($data->uridium, 0, ',', '.'); ?></span>
+                <div class="nav-stat__label">Uridium</div>
+                <div class="nav-stat__value" id="uridium"><?php echo number_format($data->uridium, 0, ',', '.'); ?></div>
               </div>
               <div class="nav-stat">
-                <span class="nav-stat__label">Credit</span>
-                <span class="nav-stat__value" id="credits"><?php echo number_format($data->credits, 0, ',', '.'); ?></span>
+                <div class="nav-stat__label">Credit</div>
+                <div class="nav-stat__value" id="credits"><?php echo number_format($data->credits, 0, ',', '.'); ?></div>
               </div>
-              <div class="nav-stat nav-stat--wide">
-                <span class="nav-stat__label">Honor + Experience</span>
-                <span class="nav-stat__value"><?php echo number_format($data->honor, 0, ',', '.'); ?> / <?php echo number_format($data->experience, 0, ',', '.'); ?></span>
+              <div class="nav-stat">
+                <div class="nav-stat__label">Experience</div>
+                <div class="nav-stat__value"><?php echo number_format($data->experience, 0, ',', '.'); ?></div>
+              </div>
+              <div class="nav-stat">
+                <div class="nav-stat__label">Honor</div>
+                <div class="nav-stat__value"><?php echo number_format($data->honor, 0, ',', '.'); ?></div>
               </div>
             </div>
           </div>
