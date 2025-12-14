17b96529b10ba68cfa78326da7d0cd2c82562dee
Acoeric98 <98672535+Acoeric98@users.noreply.github.com>
2025-12-14 04:11:39 +0100

    Add home and Ships & Boosters navigation options

---
+++ files/external/ships_boosters.php
@@ -0,0 +1,12 @@
+      <div id="main">
+        <div class="container">
+          <div class="row">
+            <div class="col s12">
+              <div class="card white-text grey darken-4 padding-15 center-align">
+                <h5>SHIPS &amp; BOOSTERS</h5>
+                <p style="margin-top: 10px;">Hajók és boosterek beszerzése folyamatban &mdash; éppen a vámon vannak, kérjük, maradjon a fedélzeten!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
