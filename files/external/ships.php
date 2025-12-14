937614102f97352b06da059fcc13f47f0fec2f28
Acoeric98 <98672535+Acoeric98@users.noreply.github.com>
2025-12-14 06:03:55 +0100

    Refresh company ship styling and mapping

--- files/external/ships.php
+++ files/external/ships.php
@@ -9,6 +9,7 @@
                   $ships = json_decode($equipment['items'])->ships;
                   array_push($ships, 8);
                   array_push($ships, 10);
+                  $companyCode = Functions::GetFactionCode($player['factionId']);
 
                   foreach ($ships as $shipId) {
                     $ship = $mysqli->query('SELECT * FROM server_ships WHERE shipID = '.$shipId.'')->fetch_assoc();
@@ -17,8 +18,17 @@
                     $lootId = Functions::ApplyCompanyShipVariant($lootId, $player['factionId']);
                     $lootId = str_replace('_', '/', $lootId);
                   ?>
-                  <div id="<?php echo $ship['lootID']; ?>" class="ship<?php echo $currentShip['baseShipId'] == $shipId ? ' active' : ''; ?>">
-                    <img src="<?php echo DOMAIN; ?>do_img/global/items/<?php echo $lootId; ?>_top.png">
+                  <div id="<?php echo $ship['lootID']; ?>" class="ship<?php echo $currentShip['baseShipId'] == $shipId ? ' active' : ''; ?><?php echo $companyCode ? ' ship--'.$companyCode : ''; ?>">
+                    <div class="ship__image" aria-hidden="true">
+                      <span class="ship__image-glow"></span>
+                      <img src="<?php echo DOMAIN; ?>do_img/global/items/<?php echo $lootId; ?>_top.png" alt="<?php echo $ship['name']; ?>">
+                    </div>
+                    <div class="ship__meta">
+                      <span class="ship__name"><?php echo $ship['name']; ?></span>
+                      <?php if ($companyCode) { ?>
+                        <span class="ship__company">Company: <?php echo strtoupper($companyCode); ?></span>
+                      <?php } ?>
+                    </div>
                   </div>
                   <?php } ?>
                 </div>
