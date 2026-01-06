29c372e6aaad76c9e093b6dca7ee0e082ce76535
Acoeric98 <98672535+Acoeric98@users.noreply.github.com>
2026-01-06 07:58:26 +0100

    Display pilot profile details

--- files/external/profile.php
+++ files/external/profile.php
@@ -3,6 +3,14 @@ $skillData = Functions::GetSkillTreeData($player['userId']);
 $titlesList = Functions::GetTitlesList();
 $currentTitleKey = $player['title'];
 $currentTitleLabel = Functions::GetTitleLabel($currentTitleKey, $titlesList);
+$playerInfo = json_decode($player['info'], true);
+$registrationDate = isset($playerInfo['registerDate']) ? $playerInfo['registerDate'] : null;
+$currentRankName = Functions::GetRankName($player['rankId']);
+$currentRankImage = DOMAIN.'img/ranks/rank_'.$player['rankId'].'.png';
+$currentRankPosition = ($player['rank'] > 0) ? $player['rank'] : null;
+$profileAvatar = DOMAIN.'img/avatar.png';
+$currentShipRow = isset($mysqli) ? $mysqli->query('SELECT name FROM server_ships WHERE shipID = '.(int)$player['shipId'].'')->fetch_assoc() : null;
+$currentShipName = $currentShipRow && isset($currentShipRow['name']) ? $currentShipRow['name'] : 'Ismeretlen hajó';
 ?>
       <div id="main" class="profile">
         <div class="container">
@@ -98,7 +106,46 @@ $currentTitleLabel = Functions::GetTitleLabel($currentTitleKey, $titlesList);
                   <div id="profile-pilot">
                     <div class="card white-text grey darken-4 padding-15">
                       <h6>Pilóta profil</h6>
-                      <p>Dummy tartalom: a pilóta részletes profilja (felszerelés, hajók, felszereltség) később kerül kitöltésre.</p>
+                      <div class="row">
+                        <div class="col s12 m4">
+                          <div class="center">
+                            <img src="<?php echo $profileAvatar; ?>" alt="Pilóta profilképe" class="responsive-img circle" style="max-width: 160px;">
+                            <p class="no-margin"><strong><?php echo htmlspecialchars($player['pilotName'], ENT_QUOTES, 'UTF-8'); ?></strong></p>
+                            <p class="grey-text text-lighten-1">Regisztráció: <?php echo $registrationDate ? htmlspecialchars($registrationDate, ENT_QUOTES, 'UTF-8') : 'Ismeretlen'; ?></p>
+                          </div>
+                        </div>
+
+                        <div class="col s12 m8">
+                          <ul class="collection grey darken-4">
+                            <li class="collection-item grey darken-3 white-text">
+                              <div class="row valign-wrapper no-margin">
+                                <div class="col s6 m4">Aktuális rang</div>
+                                <div class="col s6 m8 right-align">
+                                  <img src="<?php echo $currentRankImage; ?>" alt="<?php echo htmlspecialchars($currentRankName, ENT_QUOTES, 'UTF-8'); ?>" style="height: 28px; vertical-align: middle;"> <span><?php echo htmlspecialchars($currentRankName, ENT_QUOTES, 'UTF-8'); ?></span>
+                                </div>
+                              </div>
+                            </li>
+                            <li class="collection-item grey darken-3 white-text">
+                              <div class="row valign-wrapper no-margin">
+                                <div class="col s6 m4">Aktuális ranglista helyezés</div>
+                                <div class="col s6 m8 right-align"><?php echo $currentRankPosition ? number_format($currentRankPosition, 0, ',', '.') : 'N/A'; ?></div>
+                              </div>
+                            </li>
+                            <li class="collection-item grey darken-3 white-text">
+                              <div class="row valign-wrapper no-margin">
+                                <div class="col s6 m4">Aktuális cím</div>
+                                <div class="col s6 m8 right-align"><?php echo $currentTitleLabel ? htmlspecialchars($currentTitleLabel, ENT_QUOTES, 'UTF-8') : 'Nincs beállítva cím'; ?></div>
+                              </div>
+                            </li>
+                            <li class="collection-item grey darken-3 white-text">
+                              <div class="row valign-wrapper no-margin">
+                                <div class="col s6 m4">Jelenlegi hajó</div>
+                                <div class="col s6 m8 right-align"><?php echo htmlspecialchars($currentShipName, ENT_QUOTES, 'UTF-8'); ?></div>
+                              </div>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
                     </div>
                   </div>
 
