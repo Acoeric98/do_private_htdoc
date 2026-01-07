8b1cfa4838e4eef6e5d0a04c4d772d8668017d82
Acoeric98 <98672535+Acoeric98@users.noreply.github.com>
2026-01-08 00:59:33 +0100

    Remove inventory lootIds debug action

---
+++ flashAPI/lootids.php
@@ -0,0 +1,31 @@
+<?php
+require_once('../files/config.php');
+
+header('Content-Type: application/json; charset=utf-8');
+
+if (!Functions::IsLoggedIn()) {
+  http_response_code(401);
+  echo json_encode([
+    'error' => 'not_logged_in'
+  ]);
+  exit;
+}
+
+$mysqli = Database::GetInstance();
+
+$lootIds = [];
+$result = $mysqli->query('SELECT lootID FROM server_ships');
+if ($result) {
+  $rows = $result->fetch_all(MYSQLI_ASSOC);
+  foreach ($rows as $row) {
+    if (isset($row['lootID'])) {
+      $lootIds[] = $row['lootID'];
+    }
+  }
+}
+
+echo json_encode([
+  'count' => count($lootIds),
+  'lootIds' => $lootIds,
+  'generatedAt' => date('c')
+]);
